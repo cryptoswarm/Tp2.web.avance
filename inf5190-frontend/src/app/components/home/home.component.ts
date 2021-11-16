f997df56fab07369c1ad78dd6e7d05e3e4f29eef
cryptoswarm <mokhtar.safir@outlook.com>
2021-11-16 13:56:59 -0500

    filter by installation name

--- inf5190-frontend/src/app/components/home/home.component.ts
+++ inf5190-frontend/src/app/components/home/home.component.ts
@@ -20,18 +20,23 @@ export class HomeComponent implements OnInit {
 
   arr_name!: string;
   arr_cle!: string;
-  aqua_inst: InstallationAquatique[] = []
+  aqua_inst: InstallationAquatique[] = [];
+  aqua_inst_perm: InstallationAquatique[] = [];
   glissades: Glissade[] = [];
   patinoires: Patinoire[] = [];
   inst_names: string[] = []
   results: number = 0;
 
   searchForm: FormGroup;
+  instNamesForm: FormGroup;
 
 
   constructor(private apiClient: ApiClientService, private formBuilder: FormBuilder) {
     this.searchForm = this.formBuilder.group({
       search: ['', Validators.required]
+    })
+    this.instNamesForm = this.formBuilder.group({
+      instaName: ['', Validators.required]
     })
   }
 
@@ -48,6 +53,8 @@ export class HomeComponent implements OnInit {
       this.arr_name = installations.arr_name;
       this.arr_cle = installations.arr_cle;
       this.aqua_inst = installations.aqua_inst;
+      this.aqua_inst_perm = installations.aqua_inst;
+      console.log('this.aqua_inst :',this.aqua_inst)
       this.inst_names = this.getInstallationName(installations.aqua_inst);
       this.results = installations.aqua_inst.length;
       this.searchForm.reset()
@@ -67,5 +74,42 @@ export class HomeComponent implements OnInit {
     });
     return inst_names;
   }
+
+  // public searchEmployees(key: string):void{
+  //   console.log(key);
+  //   const result : Employee[] = [] //a new array of employees
+  //   for (const employee of this.employees){
+  //       if(employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+  //         || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+  //         || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+  //         || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+  //         result.push(employee);
+  //       }
+  //   }
+  //   this.employees =  result; //employees to be shown are those that match the search key
+  //   if(result.length === 0 || !key){  //if no employee match the search or the user has not enterned any key "!key"
+  //     this.getEmployees();
+  //   }
+  // }
+  public filterByInstallationName(): void{
+    // alert(JSON.stringify(this.instNamesForm.value))
+    const name = this.instNamesForm.value['instaName']
+    console.log('Choosen instllation name :',name)
+    const result :InstallationAquatique[] = [];
+    this.aqua_inst_perm.forEach(element => {
+      console.log('element.nom_installation: ',element.nom_installation);
+      console.log(element.nom_installation.indexOf(name) !== -1)
+      if(element.nom_installation.indexOf(name) !== -1){
+        result.push(element)
+        console.log('filtering by :'+name+' gives :', result)
+      }
+    });
+    this.aqua_inst = result;
+  }
+
+  // public onSubmit(){
+  //   //console.log('submitted')
+  //   alert(JSON.stringify(this.instNamesForm.value))
+  // }
 
 }
