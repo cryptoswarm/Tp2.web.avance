86140dcd28b15c030b7cf390f60df70d90f8e196
cryptoswarm <mokhtar.safir@outlook.com>
2021-11-24 18:31:46 -0500

    new migration

--- tests/test_config.py
+++ tests/test_config.py
@@ -37,13 +37,13 @@ class TestTestingConfig(TestCase):
         )
 
 
-# class TestProductionConfig(TestCase):
-#     def create_app(self):
-#         app.config.from_object('config.ProductionConfig')
-#         return app
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('config.ProductionConfig')
+        return app
 
-#     def test_app_is_production(self):
-#         self.assertTrue(app.config['DEBUG'] is False)
+    def test_app_is_production(self):
+        self.assertTrue(app.config['DEBUG'] is False)
 
 
 if __name__ == '__main__':
