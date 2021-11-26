f234e24b9d13a9ebf44a3bdccad2997dc054198b
cryptoswarm <mokhtar.safir@outlook.com>
2021-11-25 20:50:48 -0500

    adding 2 jobs for tes

--- worker.py
+++ worker.py
@@ -18,6 +18,14 @@ def setting_job_3():
     with app.app_context():
         persist_glissade_data()
 
+def setting_job_4():
+    with app.app_context():
+        print('This job is run every 1 min. utc time')
+
+def setting_job_5():
+    with app.app_context():
+        print('This job is run every 2 min. canada/easter')
+
 def start_working():
     scheduler = BackgroundScheduler(jobstores=app.config['JOB_STORES'], job_defaults=app.config['JOB_DEFAULTS'])
     
@@ -29,6 +37,8 @@ def start_working():
     # scheduler.add_job(func=setting_job_3, trigger='interval', minutes=5, timezone=pytz.utc)
     # scheduler.add_job(func=setting_job_3, trigger='interval', minutes=1, timezone=pytz.timezone('CANADA/EASTERN'))
     # scheduler.add_job(func=setting_job_3, trigger='interval', minutes=1, timezone=pytz.utc)
+    scheduler.add_job(func=setting_job_4, trigger='interval', minutes=1, timezone=pytz.utc)
+    scheduler.add_job(func=setting_job_5, trigger='interval', minutes=2, timezone=pytz.timezone('CANADA/EASTERN'))
     scheduler.start()
 if __name__ == '__main__':
     start_working()
