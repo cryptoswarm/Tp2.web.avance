f5cbccb2ac0e06d7a8c575267c2401f41b5abc46
cryptoswarm <mokhtar.safir@outlook.com>
2021-11-25 21:55:36 -0500

    file handler refrence befire assignment correction

--- inf5190_projet_src/__init__.py
+++ inf5190_projet_src/__init__.py
@@ -93,8 +93,8 @@ def create_app(config_name):
                 file_handler.setFormatter(logging.Formatter(
                     '%(asctime)s %(levelname)s: %(message)s '
                     '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
+                file_handler.setLevel(logging.INFO)
+                app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('data swarm start')
